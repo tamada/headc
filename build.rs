210f6db75057a3ec9089d7fd80933aaa08fc2a25
Haruaki Tamada <tamada@users.noreply.github.com>
2024-05-14 14:26:59 +0900

    enable compilation

--- build.rs
+++ build.rs
@@ -1,21 +1,25 @@
-use clap::{Command, CommandFactory}; 
+use clap::{Command, CommandFactory};
 use clap_complete::Shell;
 use std::fs::File;
 use std::path::Path;
 
 include!("src/cli.rs");
 const APP_NAME: &str = "headc"; // application name.
+
 fn generate(s: Shell, app: &mut Command, outdir: &Path, file: &str) {
-let destfile = outdir.join(file);
-println!("dest: {}", destfile.display()); std::fs::create_dir_all(destfile.parent().unwrap()).unwrap(); let mut dest = File::create(destfile).unwrap(); clap_complete::generate(s, app, APP_NAME, &mut dest);
+    let destfile = outdir.join(file);
+    println!("dest: {}", destfile.display());
+    std::fs::create_dir_all(destfile.parent().unwrap()).unwrap();
+    let mut dest = File::create(destfile).unwrap();
+    clap_complete::generate(s, app, APP_NAME, &mut dest);
 }
 
 fn main() {
-    let mut app = CliOpts::command(); 
+    let mut app = CliOpts::command();
     app.set_bin_name(APP_NAME);
     let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/completions/");
     generate(Shell::Bash, &mut app, &outdir, "bash/headc");
-    generate(Shell::Elvish,  &mut app, &outdir, "elvish/headc");
+    generate(Shell::Elvish, &mut app, &outdir, "elvish/headc");
     generate(Shell::Fish, &mut app, &outdir, "fish/headc");
     generate(Shell::PowerShell, &mut app, &outdir, "powershell/headc");
     generate(Shell::Zsh, &mut app, &outdir, "zsh/_headc");
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-fn hello() -> String {
-  "Hello, world!".to_string()
-}
+use crate::cli::CliOpts;
+use clap::Parser;
+
+mod cli;
+
 fn main() {
-  println!("{}", hello());
+    let _opts = CliOpts::parse();
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  #[test]
-  fn test_hello() {
-    assert_eq!(hello(), "Hello, world!");
-  }
+    use std::path::PathBuf;
+
+    use crate::cli::CliOpts;
+
+    use super::*;
+    #[test]
+    fn test_parse_cli() {
+        let opts = CliOpts::parse_from(&["headc_test", "-n", "5", "file1", "file2"]);
+        assert_eq!(opts.lines, Some(5));
+        assert_eq!(opts.bytes, None);
+        assert_eq!(opts.files.len(), 2);
+        assert_eq!(
+            opts.files,
+            vec![PathBuf::from("file1"), PathBuf::from("file2")]
+        );
+    }
 }
